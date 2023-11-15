4f58149df144cf0326fe777b01ddbc0dadc059b7
Josh Beveridge <git@joshbeveridge.ca>
2023-11-14 22:14:36 -0500

    Adds Netlify identity widget

--- src/index.11ty.js
+++ src/index.11ty.js
@@ -1,7 +1,29 @@
 var data = {};
 
 function render(data) {
-  return String.raw`<div></div>`;
+  return String.raw`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>A static website</title>
+      
+        <!-- include the widget -->
+        <script type="text/javascript" src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+      </head>
+      <body>
+        <!-- Add a menu:
+        Log in / Sign up - when the user is not logged in
+        Username / Log out - when the user is logged in
+        -->
+        <div data-netlify-identity-menu></div>
+      
+        <!-- Add a simpler button:
+          Simple button that will open the modal.
+        -->
+        <div data-netlify-identity-button>Login with Netlify Identity</div>
+      </body>
+    </html>
+  `;
 }
 
 module.exports = {
